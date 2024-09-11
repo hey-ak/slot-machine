d2995127036505270668aa8fb744864fbccfc946
hey-ak <akshay0127.be21@chitkara.edu.in>
2024-09-11 18:27:46 +0530

    added slot machine game

---
+++ slot-machine/slot_machineApp.swift
@@ -0,0 +1,17 @@
+//
+//  slot_machineApp.swift
+//  slot-machine
+//
+//  Created by Akshay Jha on 03/09/24.
+//
+
+import SwiftUI
+
+@main
+struct slot_machineApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
